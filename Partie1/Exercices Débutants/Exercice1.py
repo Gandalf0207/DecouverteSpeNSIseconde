2046b0bdd7d509407dab0c8aef2791b8c533d6a9
Quentin <116809947+Gandalf0207@users.noreply.github.com>
2024-12-19 10:37:34 +0100

    Update Exercice1.py
    simplification

--- Partie1/Exercices Débutants/Exercice1.py
+++ Partie1/Exercices Débutants/Exercice1.py
@@ -10,32 +10,20 @@ Concepts : Opérations arithmétiques, conditions (if, else), formatage des rés
 Exercice : Écrire un programme qui convertit une température en Celsius en Fahrenheit (et inversement) et qui renvoit cette nouvelle valeur pour qu'elle soit affichée. 
 Les élèves peuvent choisir s’ils saisissent des °C ou des °F. Completer le script suivant.
 
-Exercice Avancé (facultatif) : Documenter à l'aide de docstrings et commentaires votre programme. 
-
-Veillez à bien commenter et votre code !
-
-Completer les éléments #...
+Completer les éléments ...
 
 Formule de conversion des degrés Celsius en degrés Farenheit : C° * 9/5 + 32 = °F
 
 """
-
-def Convertisseur(temperatureType, temperature):
-    if #... : #Temperature en Celsius
-        newTemperature = # ...     # C° ->  °F
-        return f"{#... } C° = {#...} °F"
+temperatureType = int(input("Valeur saisie en : \n   Celsius : 1 \n    Fahrenheit : 2 \n\n> :")) # Choix du type de la valeur saisie (Celcius ou Fahrenheit)
+temperature = #... # Valeur à convertir
+if #... : # Comparaison temperature en Celsius
+    newTemperature = # ...     # C° ->  °F
+    print(temperature, "C° = ", newTemperature, "F°"
     
-    elif #... : # Temperature en Fahrenheit
-        newTemperature = #...    # °F -> C°
-        return f"{#...} °F = {#...} C°" 
+elif #... : # Comparaison temperature en Fahrenheit
+    newTemperature = #...    # °F -> C°
+    print(temperature, "F° = ", newTemperature, "C°"
     
-    else: # Si la valeur du type de température est différente des entrées possibles
-        print("#...") # On affiche un information d'erreur
-        return None  
-
-temperature = None
-
-temperatureType = int(input("Valeur sasie en : \n   Celsius : 1 \n    Fahrenheit : 2 \n\n> :")) # Choix du type de la valeur saisie (Celcius ou Fahrenheit)
-temperature = # ... # Valeur à convertir
-
-print(Convertisseur(temperatureType, temperature)) # Affichage + Appel de la fonction de conversion avec les valeurs de l'utilisateur
+else: # Si la valeur du type de température est différente des entrées possibles
+    print("...") # On affiche un information d'erreur
